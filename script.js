e4b7d069a94ca245a05bac4b02f4dfbef22132e3
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-17 20:41:36 -0500

    script-update-1

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+$(document).ready(function() {
 
+/* --------------- Variable Declarations --------------- */
+
+  playerTurn = 1;
+
+/* --------------- Function Declarations --------------- */
+
+  $("#top-left").on("click", function() {
+    if(playerTurn === 1) {
+      $(this).append("<div class='box-styled'>" + "X" + "</div>");
+
+      playerTurn = 2;
+    }
+    else if(playerTurn === 2) {
+      $(this).append("<div class='box-styled box-styled-player-2'>" + "O" + "</div>");
+
+      playerTurn = 1;
+    }
+  });
+
+  $("#top-mid").on("click", function() {
+    if(playerTurn === 1) {
+      $(this).append("<div class='box-styled'>" + "X" + "</div>");
+
+      playerTurn = 2;
+    }
+    else if(playerTurn === 2) {
+      $(this).append("<div class='box-styled box-styled-player-2'>" + "O" + "</div>");
+
+      playerTurn = 1;
+    }
+  });
+
+});
