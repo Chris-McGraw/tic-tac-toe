80934793b3862c8ea24b4ff80afb940c38ecb948
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-06 21:39:14 -0500

    script-update-66

--- script.js
+++ script.js
@@ -929,6 +929,18 @@ $(document).ready(function() {
           }
         }
 
+        else if($("#top-mid").children().html() === playerOneSymbol &&
+        $("#bottom-mid").children().html() === playerOneSymbol) {
+          if($("#top-right").html() === "") {
+            if(cpuSymbol === "X") {
+              $("#top-right").append("<div class='box-styled'>X</div>");
+            }
+            else if(cpuSymbol === "O") {
+              $("#top-right").append("<div class='box-styled box-styled-player-2'>O</div>");
+            }
+          }
+        }
+
       }
 
 
@@ -1399,12 +1411,97 @@ $(document).ready(function() {
           }
         }
 
+        else if($("#top-mid").children().html() === playerOneSymbol &&
+        $("#bottom-mid").children().html() === playerOneSymbol &&
+        $("#bottom-right").children().html() === playerOneSymbol) {
+          if($("#bottom-left").html() === "") {
+            if(cpuSymbol === "X") {
+              $("#bottom-left").append("<div class='box-styled'>X</div>");
+            }
+            else if(cpuSymbol === "O") {
+              $("#bottom-left").append("<div class='box-styled box-styled-player-2'>O</div>");
+            }
+          }
+        }
+
+        else if($("#top-mid").children().html() === playerOneSymbol &&
+        $("#bottom-mid").children().html() === playerOneSymbol &&
+        $("#bottom-left").children().html() === playerOneSymbol) {
+          if($("#bottom-right").html() === "") {
+            if(cpuSymbol === "X") {
+              $("#bottom-right").append("<div class='box-styled'>X</div>");
+            }
+            else if(cpuSymbol === "O") {
+              $("#bottom-right").append("<div class='box-styled box-styled-player-2'>O</div>");
+            }
+          }
+        }
+
       }
 
   /* -------------------- Turn 8 -------------------- */
       if(turnCount === 8) {
+
+      /* ----- CPU Win Logic ----- */
+
+      if($("#top-mid").children().html() === playerOneSymbol &&
+      $("#bottom-mid").children().html() === playerOneSymbol &&
+      $("#bottom-right").children().html() === playerOneSymbol &&
+      $("#top-right").children().html() === playerOneSymbol) {
+        if($("#center-left").html() === "") {
+          if(cpuSymbol === "X") {
+            $("#center-left").append("<div class='box-styled'>X</div>");
+          }
+          else if(cpuSymbol === "O") {
+            $("#center-left").append("<div class='box-styled box-styled-player-2'>O</div>");
+          }
+        }
+      }
+
+      else if($("#top-mid").children().html() === playerOneSymbol &&
+      $("#bottom-mid").children().html() === playerOneSymbol &&
+      $("#bottom-right").children().html() === playerOneSymbol &&
+      $("#center-left").children().html() === playerOneSymbol) {
+        if($("#top-right").html() === "") {
+          if(cpuSymbol === "X") {
+            $("#top-right").append("<div class='box-styled'>X</div>");
+          }
+          else if(cpuSymbol === "O") {
+            $("#top-right").append("<div class='box-styled box-styled-player-2'>O</div>");
+          }
+        }
+      }
+
+      else if($("#top-mid").children().html() === playerOneSymbol &&
+      $("#bottom-mid").children().html() === playerOneSymbol &&
+      $("#bottom-left").children().html() === playerOneSymbol &&
+      $("#top-left").children().html() === playerOneSymbol) {
+        if($("#center-right").html() === "") {
+          if(cpuSymbol === "X") {
+            $("#center-right").append("<div class='box-styled'>X</div>");
+          }
+          else if(cpuSymbol === "O") {
+            $("#center-right").append("<div class='box-styled box-styled-player-2'>O</div>");
+          }
+        }
+      }
+
+      else if($("#top-mid").children().html() === playerOneSymbol &&
+      $("#bottom-mid").children().html() === playerOneSymbol &&
+      $("#bottom-left").children().html() === playerOneSymbol &&
+      $("#center-right").children().html() === playerOneSymbol) {
+        if($("#top-left").html() === "") {
+          if(cpuSymbol === "X") {
+            $("#top-left").append("<div class='box-styled'>X</div>");
+          }
+          else if(cpuSymbol === "O") {
+            $("#top-left").append("<div class='box-styled box-styled-player-2'>O</div>");
+          }
+        }
+      }
+
       /* ----- CPU Force Tie Logic ----- */
-        if($("#top-left").children().html() === playerOneSymbol &&
+        else if($("#top-left").children().html() === playerOneSymbol &&
         $("#bottom-right").children().html() === playerOneSymbol &&
         $("#bottom-mid").children().html() === playerOneSymbol &&
         $("#top-right").children().html() === playerOneSymbol ) {
